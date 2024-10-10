30fcd95ca3bcd27472ab7431a888cc2a818915e1
Ricardo Pinheiro <rpinheiro.033@gmail.com>
2024-10-10 22:42:31 +0100

    Add built in function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,9 +1,11 @@
 const reverseString = function(str) {
-    let fStr = '';
-    for(let i = str.length - 1; i >= 0; i--) {
-        fStr += str[i];
-    }
-    return fStr;
+    // Without using bultin stuff:
+        // let fStr = '';
+        // for(let i = str.length - 1; i >= 0; i--) {
+        //     fStr += str[i];
+        // }
+        // return fStr;
+    return str.split('').reverse().join('');
 };
 
 // Do not edit below this line
