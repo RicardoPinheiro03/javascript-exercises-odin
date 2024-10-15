ef7a958ea04d4ed723aea7877699cae5684e24f7
Ricardo Pinheiro <rpinheiro.033@gmail.com>
2024-10-15 23:19:02 +0100

    Exercise 08 resolution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  const CONVERSION_CONST = 32;
+  const DIVISION_CONVERSION = 5/9;
+  const NUMBER_OF_DECIMAL_PLACES = 1;
+  return parseFloat(Number((tempFahrenheit - CONVERSION_CONST) * DIVISION_CONVERSION).toFixed(NUMBER_OF_DECIMAL_PLACES));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  const CONVERSION_CONST = 32;
+  const DIVISION_CONVERSION = 9/5;
+  const NUMBER_OF_DECIMAL_PLACES = 1;
+  return parseFloat(Number((tempCelsius * DIVISION_CONVERSION) + CONVERSION_CONST).toFixed(NUMBER_OF_DECIMAL_PLACES));
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,47 @@
-const add = function() {
-	
+const add = function(arg1, arg2) {
+  return arg1 + arg2;
 };
 
-const subtract = function() {
-	
+const subtract = function(arg1, arg2) {
+  return arg1 - arg2; 
 };
 
-const sum = function() {
-	
+const sum = function(numbArr) {
+  if(numbArr.length === 0) return 0;
+  return numbArr.reduce(sumReduction, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbArr) {
+  return numbArr.reduce(multReduction);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(arg1) {
+	if(arg1 === 0) return 1;
+  const tempFactArr = [];
+
+  for(let i = arg1; i >= 1; i--) {
+    tempFactArr.push(i);
+  }
+
+  return tempFactArr.reduce(multReduction);
 };
+
+
+/**
+ * Helper Functions
+ */
+function sumReduction(accumlator, element) {
+  return accumlator + element;
+}
+
+function multReduction(accumlator, element) {
+  return accumlator * element;
+}
+
 
 // Do not edit below this line
 module.exports = {
