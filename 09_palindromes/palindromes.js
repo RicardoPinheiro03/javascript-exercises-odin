943cc2f29dc5423e6fbb1cace1f6df7969fab9d4
Ricardo Pinheiro <rpinheiro.033@gmail.com>
2024-10-15 23:38:10 +0100

    Exercise 09 resolution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const strNoPunctuation = removePunctuation(str);
+    console.log(`No punctuation: ${strNoPunctuation}`);
+    console.log(`No punctuation and reversed: ${reverseString(strNoPunctuation)}`);
 
+    return (strNoPunctuation) === (reverseString(strNoPunctuation));
 };
+
+function removePunctuation(str) {
+    return str.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
+}
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
 
 // Do not edit below this line
 module.exports = palindromes;
