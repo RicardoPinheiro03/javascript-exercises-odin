5e1a7ebd7e76beb734e603f548fc866b0fccea45
Ricardo Pinheiro <rpinheiro.033@gmail.com>
2024-10-15 23:49:20 +0100

    Solution to exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num < 0) return "OOPS";
+    if(num <= 1) return num;
 
+    return fibonacci(num - 1) + fibonacci (num - 2);
 };
+
+/**
+ * Ignoring the cases of the strings because it accepts 8 but does
+ * n't accept other ones. Parsing the string to int also doesn't 
+ * makes sense because the solution is recursive and I'd have to 
+ * find another way to converting the type of the value.
+ */
 
 // Do not edit below this line
 module.exports = fibonacci;
